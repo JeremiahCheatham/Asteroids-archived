446ffd9314c515314016fe57e4d47729d9b059d7
JeremiahCheatham <72688505+JeremiahCheatham@users.noreply.github.com>
2022-03-15 01:39:32 +0000

    Update game_mode.h
    astroids -> asteroids

--- source/game_mode.h
+++ source/game_mode.h
@@ -4,10 +4,10 @@
 #include "common.h"
 #include "player.h"
 #include "shots.h"
-#include "astroids.h"
+#include "asteroids.h"
 #include "messages.h"
 
-enum Errors game_mode(struct Game *ast, struct Player *player, struct Shots *shots, struct Astroids *astroids, struct Messages *msg);
+enum Errors game_mode(struct Game *ast, struct Player *player, struct Shots *shots, struct Asteroids *asteroids, struct Messages *msg);
 Uint32 game_mode_timer();
 
 #endif
